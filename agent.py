cee8beeabd0e8bb13c171ac2efda7cdf8c9714b1
ds-amrit <amriteshwars590@gmail.com>
2025-05-12 19:11:15 +0530

    enhance plan generation and execution with error handling and improved logging

--- agent.py
+++ agent.py
@@ -12,6 +12,7 @@ def generate_plan(user_input: str, available_functions) -> list[Steps]:
 
 def execute_plan(plan: list[Steps], tools: list) -> Answer:
 
+    print(f"Executing plan: {plan}")
     # Execute the plan
     result = 0
     updated_input = ""
@@ -20,7 +21,7 @@ def execute_plan(plan: list[Steps], tools: list) -> Answer:
         
             function_name = step.function_name
             input = updated_input + step.step
-            print(f"\n Input: {input}")
+            print(f"\nInput: {input}")
             if function_name == "Add" and function_name in tools:
                 result = b.Add(input)
             elif function_name == "Subtract" and function_name in tools:
@@ -35,7 +36,7 @@ def execute_plan(plan: list[Steps], tools: list) -> Answer:
             updated_input =  f"The result from the previous step is: {result.answer}. Next step: "
 
         else:
-            print(f"Plan output format is incorrect: {step}")
+            print(f"Plan output format is incorrect: {type(step)}")
         
         print(f"Result of step {index}: {result.answer}")
     
@@ -43,7 +44,7 @@ def execute_plan(plan: list[Steps], tools: list) -> Answer:
 
 
 if __name__ == "__main__":
-    user_input = "Get the sum of ten and twenty and 40, then multiply it by hundred and 200"
+    user_input = "What is your name?"
 
     available_functions = [
         {
@@ -65,6 +66,10 @@ if __name__ == "__main__":
     ]
     tools = [tool["name"] for tool in available_functions]
     plan = generate_plan(user_input=user_input, available_functions=available_functions)
-    
+
+    if not plan:
+        print("Sorry plan generation failed.")
+        exit(1)
+        
     response = execute_plan(plan, tools)
     print(f"Final response: {response.answer}")
